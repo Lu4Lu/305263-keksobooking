f29abc27a7325ef26e52895ae689b5cc13aa4f91
Lu4Lu <olga.luchanok@gmail.com>
2018-06-10 23:26:57 -0500

    Mock user data.

--- js/map.js
+++ js/map.js
@@ -1,6 +1,4 @@
-/* eslint-disable */
-
-var avatars = [
+var mockAvatars = [
   'img/avatars/user01.png',
   'img/avatars/user02.png',
   'img/avatars/user03.png',
@@ -11,7 +9,7 @@ var avatars = [
   'img/avatars/user08.png'
 ];
 
-var titles = [
+var mockTitles = [
   'Большая уютная квартира',
   'Маленькая неуютная квартира',
   'Огромный прекрасный дворец',
@@ -22,26 +20,26 @@ var titles = [
   'Неуютное бунгало по колено в воде'
 ];
 
-var types = [
+var mockTypes = [
   'palace',
   'flat',
   'house',
   'bungalo'
 ];
 
-var checkInTimes = [
+var mockCheckIn = [
   '12:00',
-  '14:00',
-  '13:00'
+  '13:00',
+  '14:00'
 ];
 
-var checkOutTimes = [
+var mockCheckOut = [
   '12:00',
-  '14:00',
-  '13:00'
+  '13:00',
+  '14:00'
 ];
 
-var features = [
+var mockFeatures = [
   'wifi',
   'dishwasher',
   'parking',
@@ -50,140 +48,90 @@ var features = [
   'conditioner'
 ];
 
-var photos = [
+var mockPhotos = [
   "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 
+
 //random from range
-var min;
-var max;
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//get random array index
-var anyArray = [];
-function getRandomIndex(anyArray) {
-  return Math.floor(Math.random() * anyArray.length)
+//random array index
+function getRandomIndex(arr) {
+  return getRandomInt(0,  arr.length - 1);
 }
 
-//location
-var locationX = [];
-var locationY = [];
-
-for (var i = 0; i < 8; i++) {
-  locationX[i] = getRandomInt(300, 900);
-  locationY[i] = getRandomInt(130, 630);
+//random array item
+function getRandomItemFromArray(arr) {
+  //get an array element with the random index
+  return arr[getRandomIndex(arr)];
 }
 
-var accomodation = {};
+//remove randomly an array element
+function getRandomItemFromArrayAndRemoveItem(arr) {
+  // search for the index
+  var index = getRandomIndex(arr);
+  var item = arr[index];
+  // remove this item from array
+  arr.splice(index, 1);
+  // remove the item from array (foreeeeeveeer!)
+  return item;
+}
 
-var offers = [];
+function getRandomOrderForArray(arr) {
+  var orderArr = [];
+  var copyArr = arr.slice();
+  //make the cycle for random amount of items
+  for (var i = 0; i < arr.length; i++) {
+    //add sequentially the items initial array to the new one
+    orderArr.push(getRandomItemFromArrayAndRemoveItem(copyArr));
+    // search for the index
+  }
+  return orderArr;
+}
 
-var homeAuthors = [];
-var homeTitles = [];
-var homePrices = [];
-var homeAddresses = [];
-var homeTypes = [];
-var homeRooms = [];
-var homeGuests = [];
-var homeCheckInTimes = [];
-var homeCheckOutTimes = [];
-var homeFeatures = [];
-var homeDescriptions = [];
-var homePhotos = [];
+function getRandomItemsFromArray(arr) {
+  // amount of items
+  var items = getRandomInt(1, arr.length);
+  // create a new array with this items
+  return getRandomOrderForArray(arr).slice(0, items);
+}
+
+var appartments = [];
 
 for (var i = 0; i < 8; i++) {
-
-  // author
-  var randomAuthorIndex = getRandomIndex(avatars);
-  homeAuthors[i] = avatars[randomAuthorIndex];
-  accomodation.author = homeAuthors[i];
-  avatars.splice(randomAuthorIndex, 1);
-
-  //title
-  var randomTitleIndex = getRandomIndex(titles);
-  homeTitles[i] = titles[randomTitleIndex];
-  titles.splice(randomTitleIndex, 1);
-  // offers.push(homeTitles[i]);
-
-  //price
-  homePrices[i] = getRandomInt(1000, 1000000);
-  // offers.push(homePrices[i]);
-
-  //address
-  homeAddresses[i] = locationX[i] + ', ' + locationY[i];
-  // offers.push(homeAddresses[i]);
-
-  //type
-  var randomTypeIndex = getRandomIndex(types);
-  homeTypes[i] = types[randomTypeIndex];
-  // offers.push(homeTypes[i]);
-
-  //rooms amount
-  homeRooms[i] = getRandomInt(1, 5);
-  // offers.push(homeRooms[i]);
-
-  //guest amount
-  homeGuests[i] = getRandomInt(1, 100);
-  // offers.push(homeGuests[i]);
-
-  //check-in time
-  var randomCheckInIndex = getRandomIndex(checkInTimes);
-  homeCheckInTimes[i] = checkInTimes[randomCheckInIndex];
-  // offers.push(homeCheckInTimes[i]);
-
-  //check-out time
-  var randomCheckOutIndex = getRandomIndex(checkOutTimes);
-  homeCheckOutTimes[i] = checkOutTimes[randomCheckOutIndex];
-  // offers.push(homeCheckOutTimes[i]);
-
-  //features array
-  var tempFeatures = features.slice();
-  //random amount of feature
-  var featureAmount = getRandomInt(1, 6);
-  for (var j = 0; j <= featureAmount - 1; j++) {
-    //random for the exact feature
-    var randomFeatureIndex = getRandomIndex(tempFeatures);
-    homeFeatures[j] = tempFeatures[randomFeatureIndex];
-    tempFeatures.splice(randomFeatureIndex, 1);
+  var location = {
+    x: getRandomInt(300, 900),
+    y: getRandomInt(130, 630)
   }
 
-  //photo links
-  var tempPhotos = photos.slice();
-  // console.log(tempPhotos);
-  for (var j = 0; j < photos.lenght; j++) {
-    var randomPhotoIndex = getRandomIndex(tempPhotos);
-    console.log(randomPhotoIndex);
-    homePhotos[j] = tempPhotos[randomPhotoIndex];
-    console.log(homePhotos[j]);
-    tempPhotos.splice(randomPhotoIndex, 1);
-  }
+  // object with all accomodation data
+  var accomodation = {
+    author: {
+      avatar: getRandomItemFromArrayAndRemoveItem(mockAvatars)
+    },
 
-  //description
-  homeDescriptions[i] = '___________';
-  // offers.push(homeDescriptions[i]);
+    offer: {
+      title: getRandomItemFromArrayAndRemoveItem(mockTitles),
+      address: location.x + ', ' + location.y,
+      price: getRandomInt(1000, 1000000),
+      type: getRandomItemFromArray(mockTypes),
+      rooms: getRandomInt(1, 5),
+      guests: getRandomInt(1, 20),
+      checkin: getRandomItemFromArray(mockCheckIn),
+      checkout: getRandomItemFromArray(mockCheckOut),
+      features: getRandomItemsFromArray(mockFeatures),
+      description: ' ',
+      photos: getRandomOrderForArray(mockPhotos)
+    },
 
-  //all info in offer
-  offers[i] = [homeTitles[i],
-    homePrices[i],
-    homeAddresses[i],
-    homePrices[i],
-    homeTypes[i],
-    homeRooms[i],
-    homeFeatures,
-    homeDescriptions[i],
-    homePhotos
-  ];
+    location: location
+  };
 
-  accomodation.offer = offers[i];
-
-  accomodation.location = homeAddresses[i];
-
-  console.log(i);
   console.log(accomodation);
+  appartments.push(accomodation);
 }
-
-/* eslint-enable */
