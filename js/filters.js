38da0d466f772ea202957371eff025650d553cc0
Lu4Lu <olga.luchanok@gmail.com>
2018-06-29 15:42:59 -0500

    module7-task1 / all filters in / corrections required

--- js/filters.js
+++ js/filters.js
@@ -1,24 +1,13 @@
 'use strict';
 (function () {
 
-  // var MIN_FILTER_PRICE = 10000;
-  // var MAX_FILTER_PRICE = 50000;
+  var MIN_FILTER_PRICE = 10000;
+  var MAX_FILTER_PRICE = 50000;
   var newPins = [];
 
   var mapFiltersForm = document.querySelector('.map__filters');
-  var housingTypeElement = mapFiltersForm.querySelector('#housing-type');
 
-  // window.appartments = [];
-  // window.load(
-  //   function (data) {
-  //     window.appartments = data;
-  //   },
-  //   window.onError
-  // );
-
-  // copy my initial array
   // render first the basic pins array from data
-
   // compare the filter of the element and push this element to the array
   // show the new array
 
@@ -27,6 +16,7 @@
 
   // here are filters
   function filterHousingType(pin) {
+    var housingTypeElement = mapFiltersForm.querySelector('#housing-type');
     switch (housingTypeElement.value) {
       case 'any':
         return pin;
@@ -35,13 +25,61 @@
     }
   }
 
+
+  function filterPrice(pin) {
+    var housingPriceElement = mapFiltersForm.querySelector('#housing-price');
+    switch (housingPriceElement.value) {
+      case 'any':
+        return pin;
+      case 'low':
+        return pin.offer.price <= MIN_FILTER_PRICE;
+      case 'middle':
+        return pin.offer.price >= MIN_FILTER_PRICE && pin.offer.price <= MAX_FILTER_PRICE;
+      case 'high':
+        return pin.offer.price >= MAX_FILTER_PRICE;
+      default:
+        return pin.offer.price === housingPriceElement.value;
+    }
+  }
+
+  function filterRooms(pin) {
+    var housingRoomsElement = mapFiltersForm.querySelector('#housing-rooms');
+    switch (housingRoomsElement.value) {
+      case 'any':
+        return pin;
+      default:
+        return pin.offer.rooms === parseInt(housingRoomsElement.value, 10);
+    }
+  }
+
+  function filterGuests(pin) {
+    var housingGuestsElement = mapFiltersForm.querySelector('#housing-guests');
+    switch (housingGuestsElement.value) {
+      case 'any':
+        return pin;
+      default:
+        return pin.offer.guests === parseInt(housingGuestsElement.value, 10);
+    }
+  }
+
+  function filterFeatures(pin) {
+    var housingFeatures = mapFiltersForm.querySelectorAll('.map__checkbox');
+
+    for (var i = 0; i < housingFeatures.length; i++) {
+      if (housingFeatures[i].checked && pin.offer.features.indexOf(housingFeatures[i].value) < 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   function deletePins() {
     var pinsContainerElement = document.querySelector('.map__pins');
     var pinElement = pinsContainerElement.querySelectorAll('.map__pin');
-
-    // pinsContainerElement.forEach(pin) {
-    //   pinsContainerElement.removeChild(pinElement[i])
-    // }
+    //
+    // pinsContainerElement.forEach(function (pin) {
+    //   pinsContainerElement.removeChild(pin);
+    // });
     for (var i = 1; i < pinElement.length; i++) {
       pinsContainerElement.removeChild(pinElement[i]);
     }
@@ -49,13 +87,11 @@
 
   // take this array and map the new one with filters
   function filteredPins() {
+    // copy my initial array
     newPins = window.appartments.slice();
 
-    var filtered = newPins.filter(filterHousingType);
-
-    // var houseType = newPins.filter(function (it) {
-    //   return it.offer.type === housingTypeElement.value;
-    // });
+    var filtered = newPins.filter(filterHousingType).filter(filterPrice)
+      .filter(filterRooms).filter(filterGuests).filter(filterFeatures);
 
     deletePins();
     window.renderUserPins(filtered);
