94e42c1c61f2ff100e7fe163314f5151e0e7074e
Lu4Lu <olga.luchanok@gmail.com>
2018-07-03 15:00:58 -0500

    final review / 2.6 required on the room number and capacity fieldsets

--- js/map.js
+++ js/map.js
@@ -3,15 +3,13 @@
 // Pin interaction with web site
 //
 var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 window.mapElement = document.querySelector('.map');
 var mainPinElement = window.mapElement.querySelector('.map__pin--main');
 var adFormContainerElement = document.querySelector('.ad-form');
 var closeErrorMessageElement = document.querySelector('.error__message--close');
 
 window.isMapActive = false;
-
-// window.appartments = [];
-
 
 // actions on main pin release
 function releaseMainPin() {
@@ -71,6 +69,13 @@ closeErrorMessageElement.addEventListener('click', function () {
 document.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
     closeErrorMessage();
+  }
+});
+
+// click on main pin
+mainPinElement.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    releaseMainPin();
   }
 });
 
